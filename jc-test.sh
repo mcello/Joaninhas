ba20f75d004f7371ab553eb4105fad205e2dea24
mcello <mcellor210@gmail.com>
2014-12-05 17:25:02 -0200

    [mac0431.22014] - Scritp de teste melhorado.

--- jc-test.sh
+++ jc-test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#rm joaninhasCalorosas ; make -k joaninhasCalorosas ; /usr/bin/time ./joaninhasCalorosas 10 10 12 0 1 1 1 0.0 1 0.0 1 1000 2 && cp ./jc-out.txt ./jc2-out.txt && /usr/bin/time ./joaninhasCalorosas 10 10 12 0 1 1 1 0.0 1 0.0 1 1000 1 && diff ./jc-out.txt ./jc2-out.txt
+#rm joaninhasCalorosas ; make -k joaninhasCalorosas ; 
+#time ./joaninhasCalorosas 10 10 12 0 1 1 1 0.0 1 0.0 1 1000 2 && cp ./jc-out.txt ./jc2-out.txt && 
+#time ./joaninhasCalorosas 10 10 12 0 1 1 1 0.0 1 0.0 1 1000 1 && diff ./jc-out.txt ./jc2-out.txt
 
 rm joaninhasCalorosas ; make -k joaninhasCalorosas ; 
 /usr/bin/time ./joaninhasCalorosas 100 100 120 42 30 27 38 0.4 2 0.4 3 1000 1 && cp ./jc-out.txt ./jc1-out.txt && \
@@ -9,5 +11,3 @@ rm joaninhasCalorosas ; make -k joaninhasCalorosas ;
 /usr/bin/time ./joaninhasCalorosas 100 100 120 42 30 27 38 0.4 2 0.4 3 1000 8 && cp ./jc-out.txt ./jc8-out.txt && \
 diff ./jc1-out.txt ./jc8-out.txt
 
-#rm joaninhasCalorosas ; make -k joaninhasCalorosas ; /usr/bin/time ./joaninhasCalorosas 10 10 10 42 30 27 38 0.4 2 0.4 3 1 2 && cp ./jc-out.txt ./jc2-out.txt && \
-#                                                     /usr/bin/time ./joaninhasCalorosas 10 10 10 42 30 27 38 0.4 2 0.4 3 1 1 && diff ./jc-out.txt ./jc2-out.txt
